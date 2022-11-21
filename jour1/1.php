38475bc063cbe409fb0fd28121e69ab9dba063ac
gabriel-rigaud <114390299+gabriel-rigaud@users.noreply.github.com>
2022-11-21 13:59:23 +0100

    Add files via upload

---
+++ jour1/1.php
@@ -0,0 +1,16 @@
+<?php
+$str = "LaPlateforme ";
+$str1 = "Vive ";
+$str2 = "! <br/><br/>";
+$myBool = true;
+
+echo "$str1 $str $str2 <br/>";
+echo " Ma concaténation =". $str1, $str, $str2;
+
+$val = 6;
+echo " $val<br/>";
+
+$val += 4;
+echo " $val<br/>";
+
+echo "Mon Bool Est : $myBool";
---
+++ jour1/2.php
@@ -0,0 +1,7 @@
+<?php
+//Le texte a imprimé
+$txt = "Hello LaPlateforme!";
+//Premier print "imprime" on va le faire 2 fois de la même façon
+print $txt. '<br/>';
+//Second print "imprime" pour avoir 2 fois le texte
+print $txt. '<br/>';
