d69c088925556424e8b317c812a227ca032607b1
gabriel-rigaud <114390299+gabriel-rigaud@users.noreply.github.com>
2022-11-22 15:38:28 +0100

    Add files via upload

--- jour7/Job6.php
+++ jour7/Job6.php
@@ -5,28 +5,22 @@ function leetspeak($str){
         if ($str[$a] == 'A' || $str[$a] == 'a') {
             $str[$a] = '4';
         }
-        else
-            if ($str[$a] == 'B' || $str[$a] == 'b') {
+        else if ($str[$a] == 'B' || $str[$a] == 'b') {
                 $str[$a] = '8';
             }
-            else
-                if ($str[$a] == 'E' || $str[$a] == 'e') {
+            else if ($str[$a] == 'E' || $str[$a] == 'e') {
                     $str[$a] = '3';
                 }
-                else
-                    if ($str[$a] == 'G' || $str[$a] == 'g') {
+                else if ($str[$a] == 'G' || $str[$a] == 'g') {
                         $str[$a] = '6';
                     }
-                    else
-                        if ($str[$a] == 'L' || $str[$a] == 'l') {
+                    else if ($str[$a] == 'L' || $str[$a] == 'l') {
                             $str[$a] = '1';
                         }
-                        else
-                            if ($str[$a] == 'S' || $str[$a] == 's') {
+                        else if ($str[$a] == 'S' || $str[$a] == 's') {
                                 $str[$a] = '5';
                             }
-                            else
-                                if ($str[$a] == 'T' || $str[$a] == 't') {
+                            else if ($str[$a] == 'T' || $str[$a] == 't') {
                                     $str[$a] = '7';
                                 }
         $a++;
