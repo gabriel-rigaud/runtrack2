29bbad598d27cca8e484b205043501ad4951e3bd
gabriel-rigaud <114390299+gabriel-rigaud@users.noreply.github.com>
2022-11-25 02:22:26 +0100

    Add files via upload

---
+++ Jour9/job16.sql
@@ -0,0 +1,4 @@
+//Ecrivez dans le fichier “job16.sql” une requête permettant de récupérer le nom de
+l'étage ayant la salle avec la plus grande capacité (et afficher aussi le nom de cette salle
+ainsi que sa capacité).
+Dans ce résultat, la colonne “nom” de la salle doit être renommée en “Biggest Room”//
